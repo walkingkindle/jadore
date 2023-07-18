754eebe58d7dc85be4e9bf87671b4363a725db4f
UrosZikic <uroszikic1512988@gmail.com>
2023-07-18 08:26:24 +0200

    front-end files commit

---
+++ jadore/src/script.js
@@ -0,0 +1,27 @@
+// const navigation = document.querySelector(".navigation");
+// const navContent = document.querySelector(".nav-content");
+// const menu = document.querySelector("#menu");
+
+// function widthChange() {
+//   if (navigation.offsetWidth <= 1140) {
+//     navContent.style.display = "none";
+//     menu.style.display = "inline-block";
+//   } else {
+//     navContent.style.display = "block";
+//     menu.style.display = "none";
+//   }
+// }
+
+// window.addEventListener("load", widthChange);
+// window.addEventListener("resize", widthChange);
+
+// const menuSlide = document.querySelector(".menu-slide");
+// const menuDisapper = document.querySelector(".close-menu");
+// function openMenu() {
+//   // menuSlide.style.display = "flex";
+//   menuSlide.style.transform = "translateX(0rem)";
+// }
+// function closeMenu() {
+//   // menuSlide.style.display = "none";
+//   menuSlide.style.transform = "translateX(25rem)";
+// }
