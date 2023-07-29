a7a17d6e1514b6304dbcc17a9a3909b37d956292
UrosZikic <uroszikic1512988@gmail.com>
2023-07-29 16:22:39 +0200

    landing page update

--- jadore/src/FE scripts/script.js
+++ jadore/src/FE scripts/script.js
@@ -1,11 +1,10 @@
-const navigation = document.querySelector(".bottom-nav");
-const openMenu = document.querySelector(".open-menu");
-const slider = document.querySelector(".slider-menu");
-
+var navigation = document.querySelector(".bottom-nav");
+var openMenu = document.querySelector(".open-menu");
+var slider = document.querySelector(".slider-menu");
 function widthChange() {
   if (document.body.offsetWidth <= 900) {
     navigation.style.display = "none";
-    openMenu.style.display = "inline-block";
+    openMenu.style.display = "inline-block"; // Fix: set display property correctly
   } else {
     slider.style.transform = "translateX(-27rem)";
     navigation.style.display = "grid";
@@ -14,22 +13,37 @@ function widthChange() {
 }
 window.addEventListener("load", widthChange);
 window.addEventListener("resize", widthChange);
-
 function oMenu() {
   slider.style.transform = "translateX(0rem)";
 }
-
 function cMenu() {
   slider.style.transform = "translateX(-27rem)";
 }
 
-// const opened = document.querySelector(".open-close");
+// best seller
 
-// var x = new Date();
-// var y = x.getHours();
+const multipleItemCarousel = document.querySelector("#carouselExampleControls");
 
-// if (y >= 8 && y < 20) {
-//   opened.innerHTML = "We're currently open!";
-// } else {
-//   opened.innerHTML = "We're closed!";
-// }
+if (window.matchMedia("(min-width:576px)").matches) {
+  var carouselWidth = $(".best-seller--inner")[0].scrollWidth;
+  var cardWidth = $(".best-seller--item").width();
+
+  var scrollPosition = 0;
+
+  $(".carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      //check if you can go any further
+      scrollPosition += cardWidth; //update scroll position
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
+    }
+  });
+
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+} else {
+  $(multipleItemCarousel).addClass("slide");
+}
