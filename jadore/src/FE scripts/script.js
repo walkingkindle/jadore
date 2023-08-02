439c23c3bf6a500ba91367589e5149ab7d1a8f1a
UrosZikic <uroszikic1512988@gmail.com>
2023-08-02 10:01:18 +0200

    landing page update

--- jadore/src/FE scripts/script.js
+++ jadore/src/FE scripts/script.js
@@ -34,14 +34,14 @@ if (window.matchMedia("(min-width:576px)").matches) {
     if (scrollPosition < carouselWidth - cardWidth * 4) {
       //check if you can go any further
       scrollPosition += cardWidth; //update scroll position
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800); //scroll left
     }
   });
 
   $(".carousel-control-prev").on("click", function () {
     if (scrollPosition > 0) {
       scrollPosition -= cardWidth;
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800);
     }
   });
 } else {
