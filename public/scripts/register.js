dddc4b76bff39ff5ff7d8fc5e6a75d6f8df2f5e9
UrosZikic <uroszikic1512988@gmail.com>
2023-08-06 20:24:38 +0200

    nav.hbs log in update

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,11 +1,27 @@
+/* eslint-disable prettier/prettier */
 const curtain = document.querySelector('.register-curtain');
-const registerBox = document.querySelector('.register');
+const registerBox = document.querySelector('.sign-modal');
 const signUp = document.querySelector('.sign-up');
+const signIn = document.querySelector('.sign-in');
+const signUpModal = document.getElementById('register');
+const signInModal = document.getElementById('log-in');
+// sign-up
 signUp.onclick = () => {
   curtain.style.transform = "translateX(0%)";
+  signUpModal.style.display = "flex";
+  signInModal.style.display = "none";
 }
+
+// sing-in 
+signIn.onclick = () => {
+  curtain.style.transform = "translateX(0%)";
+  signInModal.style.display = "flex";
+  signUpModal.style.display = "none";
+}
+
+// curtain behavior
 curtain.addEventListener("click", (event) => {
-  if (!registerBox.contains(event.target)) {
+  if (!signUpModal.contains(event.target) && !signInModal.contains(event.target)) {
     curtain.style.transform = "translateX(100%)";
   }
 })
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,28 +1,29 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import * as dotenv from 'dotenv'
-import * as nodemailer from 'nodemailer'
+import * as dotenv from 'dotenv';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
-    private transporter:nodemailer.Transporter
+  private transporter: nodemailer.Transporter;
 
-    constructor(){
-        this.transporter = nodemailer.createTransport({
-            service:'Gmail',
-            auth:{
-                user: process.env.MAIL_MAIL,
-                pass:process.env.MAIL_PASSWORD,
-            },
-        });
-    }
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.MAIL_MAIL,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
 
-    async sendActivationMail(to:string,activationToken:string):Promise<void>{
-        const activationLink = `127.0.0.0:3000/register/activate/${activationToken}`
-        this.transporter.sendMail({
-            from: process.env.MAIL_MAIL,
-            to,
-            subject:'Account Activation',
-            html:`<p> Click on the following link to activate your account.</p><a href="${activationLink}">${activationLink}</a>`,
-        })
-    }
+  async sendActivationMail(to: string, activationToken: string): Promise<void> {
+    const activationLink = `127.0.0.0:3000/register/activate/${activationToken}`;
+    this.transporter.sendMail({
+      from: process.env.MAIL_MAIL,
+      to,
+      subject: 'Account Activation',
+      html: `<p> Click on the following link to activate your account.</p><a href="${activationLink}">${activationLink}</a>`,
+    });
+  }
 }
