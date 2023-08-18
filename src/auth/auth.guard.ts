4f84a3950e83383be2c20bcf292583890ebbf0ff
UrosZikic <uroszikic1512988@gmail.com>
2023-08-18 12:30:04 +0200

    activation, pass reset, etc. message update

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,36 +1,38 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { jwtConstants } from './auth.constants';
-import {JwtService} from '@nestjs/jwt'
+import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { AuthService } from './auth.service'; // Your authentication service
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService:JwtService){}
-  async canActivate(
-    context: ExecutionContext):Promise <boolean>{
-    const request = context.switchToHttp().getRequest()
-    const token = this.extractTokenFromHeader(request)
-    if (!token){
-      throw new UnauthorizedException()
-    }
-    try{
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        {
-          secret: jwtConstants.secret
-        }
-      )
-
-      request['user'] = payload
-    }catch{
+  constructor(private jwtService: JwtService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
       throw new UnauthorizedException();
     }
-    return true
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
 
-  }  
-  private extractTokenFromHeader(request:Request):string|undefined{
-    const[type,token] = request.headers.authorization?.split(' ')?? []
-    return type === 'Bearer' ? token:undefined
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
   }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,148 +1,170 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Request, Response, Render, Param, Redirect, Res, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  Render,
+  Param,
+  Redirect,
+  Res,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { error } from 'console';
 import { AuthService } from 'src/auth/auth.service';
-import { AuthGuard } from  '../auth/auth.guard';
-
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('user')
 export class UserController {
-    constructor(
-        private readonly userService:UserService,
-        ){}
+  constructor(private readonly userService: UserService) {}
 
-
-    @Get('/sign-up')
-    @Render('sign-up')
-    getSignUp() {
-      let data = {
-        title: 'Sign-up',
-      };
-      return data;
-    }
-
-    @Post('register')
-    async register(
-        @Request() req,
-        @Response() res,
-
-    ):Promise<void>{
-        const { name, email,password} = req.body
-        const user = await this.userService.FindByEmail(email)
-        if(user){
-            // res.redirect('/user/register')
-            res.render('index')
-        } else{
-           
-            await this.userService.create(name,email,password)
-            const stil = "padding: 5rem;";
-            const logMessage = "Check your email for confirmation code!";
-            res.render('index', {logMessage, stil }) //handle
-            // res.redirect('index')
-        }
-    }
-    @Post('/login')
-    async login(@Request() req, @Response() res): Promise<void> {
-        const { email, password } = req.body;
-        const user = await this.userService.FindByEmail(email);
-    
-        if (user && user.isActivated) {
-            const isPasswordValid = await this.userService.verifyPassword(user, password);
-    
-            if (!isPasswordValid) {
-                return res.render('index', { logMessage: "Wrong password. Please try again." });
-            }
-            
-            const payload = { sub: user.id, email: user.email };
-            const token = await this.userService.generateToken(payload);
-            
-            return res.render('index', { token });  
-        } else {
-            return res.render('index', { logMessage: "User not found" });
-        }
-    }
-    
-
-
-@UseGuards(AuthGuard)
-@Get('profile')
-getProfile(@Request() req){
-    return req.user
-}
-
-// 
-@Get('/activate/:activationToken')
-async activate(@Param('activationToken') activationToken: string, @Response() res): Promise<void> {
-  try {
-    const isActivated = await this.userService.activateAccount(activationToken);
-    console.log(isActivated)
-    res.redirect('/user/activated/true'); // Updated redirection
-  } catch {
-    console.log(error);
-    res.redirect("/user/activated/false"); // Updated redirection
+  @Get('/sign-up')
+  @Render('sign-up')
+  getSignUp() {
+    const data = {
+      title: 'Sign-up',
+    };
+    return data;
   }
-}
 
+  @Post('register')
+  async register(@Request() req, @Response() res): Promise<void> {
+    const { name, email, password } = req.body;
+    const user = await this.userService.FindByEmail(email);
+    if (user) {
+      // res.redirect('/user/register')
+      res.render('index');
+    } else {
+      await this.userService.create(name, email, password);
+      const stil = 'padding: 5rem;';
+      const logMessage = 'Check your email for confirmation code!';
+      res.render('index', { logMessage, stil }); //handle
+      // res.redirect('index')
+    }
+  }
+  @Post('/login')
+  async login(@Request() req, @Response() res): Promise<void> {
+    const { email, password } = req.body;
+    const user = await this.userService.FindByEmail(email);
 
-@Get('/activated/:activated')
-async handleActivated(@Param('activated') activated: string, @Response() res): Promise<void> {
+    if (user && user.isActivated) {
+      const isPasswordValid = await this.userService.verifyPassword(
+        user,
+        password,
+      );
+
+      if (!isPasswordValid) {
+        return res.render('index', {
+          logMessage: 'Wrong password. Please try again.',
+        });
+      }
+
+      const payload = { sub: user.id, email: user.email };
+      const token = await this.userService.generateToken(payload);
+
+      return res.render('index', { token });
+    } else {
+      return res.render('index', { logMessage: 'User not found' });
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  //
+  @Get('/activate/:activationToken')
+  async activate(
+    @Param('activationToken') activationToken: string,
+    @Response() res,
+  ): Promise<void> {
+    try {
+      const isActivated = await this.userService.activateAccount(
+        activationToken,
+      );
+      console.log(isActivated);
+      res.redirect('/user/activated/true'); // Updated redirection
+    } catch {
+      console.log(error);
+      res.redirect('/user/activated/false'); // Updated redirection
+    }
+  }
+
+  @Get('/activated/:activated')
+  async handleActivated(
+    @Param('activated') activated: string,
+    @Response() res,
+  ): Promise<void> {
     let logMessage = '';
     let stil = '';
-    
+
     if (activated === 'true') {
-        logMessage = 'Account activated successfully!';
-        stil = 'padding: 5rem;';
+      logMessage = 'Account activated successfully!';
+      stil = 'padding: 5rem;';
     } else if (activated === 'false') {
-        logMessage = 'Failed to activate account.';
+      logMessage = 'Failed to activate account.';
     } else {
-        logMessage = 'Invalid activation request.';
+      logMessage = 'Invalid activation request.';
     }
 
     res.render('index', { logMessage, stil });
-}
-@Post("sendActivationMail")
-async sendResetPasswordMail(@Request() req, @Response() res):Promise<void>{
-    const {email} = req.body;
-    this.userService.sendForgotPasswordMail(email)
-    return res.render("index") // Display a notification saying your email has been sent or something.
-}
+  }
+  @Post('sendActivationMail')
+  async sendResetPasswordMail(@Request() req, @Response() res): Promise<void> {
+    let logMessage = '';
+    let stil = '';
+    const { email } = req.body;
+    this.userService.sendForgotPasswordMail(email);
+    logMessage = 'Check your email for activation link';
+    stil = 'padding: 5rem';
+    return res.render('index', { logMessage, stil }); // Display a notification saying your email has been sent or something.
+  }
 
-@Get("resetPassword/:activationLink/:id")
-async resetPasswordPage(@Request() req,@Response() res,@Param('activationLink') activationLink:string,@Param('id') id:number):Promise<void>{
-    const isOkay = this.userService.checkActivationLinkAndId(id,activationLink)
-    if(!isOkay){
-        //display a notification here that link is invalid.
-        console.log("link invalid")
+  @Get('resetPassword/:activationLink/:id')
+  async resetPasswordPage(
+    @Request() req,
+    @Response() res,
+    @Param('activationLink') activationLink: string,
+    @Param('id') id: number,
+  ): Promise<void> {
+    const isOkay = this.userService.checkActivationLinkAndId(
+      id,
+      activationLink,
+    );
+    let logMessage = '';
+    let stil = '';
+    if (!isOkay) {
+      logMessage = 'The activation link is not valid';
+      stil = 'padding: 5rem';
+      console.log('link invalid');
+      return res.render('index', { logMessage, stil }); //display a notification here that link is invalid.
     }
-    return res.render("resetPass",{id:id})
-}
-@Post("changeMyPassword/:id")
-async changePassword(@Request() req, @Response() res,@Param("id") id:number ):Promise<void>{
-    const {password} = req.body
-    console.log("Here is the id",id)
-    this.userService.changePassword(password,id)
+    return res.render('resetPass', { id: id });
+  }
+  @Post('changeMyPassword/:id')
+  async changePassword(
+    @Request() req,
+    @Response() res,
+    @Param('id') id: number,
+  ): Promise<void> {
+    const { password } = req.body;
+    console.log('Here is the id', id);
+    this.userService.changePassword(password, id);
+    let logMessage = '';
+    let stil = '';
     //Display here a message that that was a sucess.
-    return res.render("index")
-
+    logMessage = 'Password changed successfully';
+    stil = 'padding: 5rem';
+    return res.render('index', { logMessage, stil });
+  }
+  @Post('/logout')
+  async logout(@Response() res): Promise<void> {
+    res.clearCookie('access_token');
+    return res.redirect('/');
+  }
 }
-@Post("/logout")
-async logout(@Response() res):Promise<void>{
-    res.clearCookie('access_token')
-    return res.redirect("/")
-}
-}
-
-
-
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
